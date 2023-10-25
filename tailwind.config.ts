61ee2a961df2c1bfccc498b6679234ab34e063ec
Cesar <cesarcanton64@gmail.com>
2023-10-24 20:04:00 -0600

    Merge de dev

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'theme-dark-blue': '#0663BA',
+        'theme-blue': '#66B2FF',
+        'theme-green': '#00CC66',
+        'theme-red': '#FF6666',
+        'theme-orange': '#FFB570'
+      }
     },
   },
   plugins: [],
