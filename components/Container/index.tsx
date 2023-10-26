ac59c8c424e17da4a25c8068c5d51cdf5b8b8916
Diego Quinteros <ql17001@ues.edu.sv>
2023-10-26 12:35:24 -0600

    💄 Update style

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -7,7 +7,7 @@ interface Chil{
 
 const Container = ({children}:Chil) => {
 return (
-<div className='flex-1 max-w-6xl m-auto flex-wrap border border-gray-700 rounded-lg w-full my-8'>
+<div className='flex-1 max-w-6xl m-auto flex-wrap border border-gray-700 rounded-lg w-full my-8 p-8'>
     
         {children}
         
