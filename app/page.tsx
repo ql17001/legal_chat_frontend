491e0ff3dcdd3c55f7d4c710cc11ba376716ea9c
Jose Garcia <GT22004@ues.edu.sv>
2023-11-08 00:07:00 -0600

    Pagina Home Dinamica 🏰

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,29 @@
 'use client'
-import Pagination from '@/components/Pagination'
-import Image from 'next/image'
-
+import HomeAdmin from '@/components/HomeAdmin';
+import HomeAdviser from '@/components/HomeAdviser';
+import HomeClient from '@/components/HomeClient';
+import { Routes } from '@/utils/constants'
+import { deleteAuthentication, retrieveAuthentication } from '@/utils/authentication';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [role, setRole] = useState<string>()
+  const pathname = usePathname()
+
+  useEffect(() => {
+    const authentication = retrieveAuthentication();
+
+    if(authentication){
+      setRole(authentication.role);
+    }
+  }, [pathname])
+
   return (
     <main>
-      Pagina inicio
-      <div className='flex flex-col items-start gap-2'>
-        <input type='text' value={'input default'}/>
-
-        <button>Boton default</button>
-        
-        <button className=' boton-error'>Error</button>
-        
-        <button className=' boton-guardar'>Guardar</button>
-        
-        <button className='boton-editar'>Editar</button>
-
-        <Pagination currentPage={7} totalPages={20} onClick={(newPage) => alert(newPage)}/>
-      </div>
+      {(role==='Administrador') && <HomeAdmin />}
+      {(role==='Cliente') && <HomeClient />}
+      {(role==='Asesor') && <HomeAdviser />}
     </main>
   )
 }
