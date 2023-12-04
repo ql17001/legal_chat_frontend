694595e3f36c1d1cd0dc81e1ca10543469700fc3
Diego Quinteros <ql17001@ues.edu.sv>
2023-12-04 11:17:24 -0600

    💄 Update ChatsContainer UI

--- components/ChatsContainer/index.tsx
+++ components/ChatsContainer/index.tsx
@@ -1,6 +1,6 @@
 import { IMensaje } from '@/types/global';
 import dateFormater from '@/utils/dateFormater';
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 interface IProperties {
   mensajes: IMensaje[];
@@ -11,18 +11,30 @@ interface IChatItemProperties {mensaje: IMensaje; mio: boolean}
 
 const ChatItem = ({mensaje, mio}: IChatItemProperties) => {
   const { dateString, timeString } = dateFormater(mensaje.fechaEnvio.date)
-  return <div className={`flex ${mio ? 'flex-row self-end':'flex-row-reverse self-start'} gap-2 max-w-sm`}>
-    <div className='flex flex-col items-center text-xs text-gray-400 justify-center'><span>{dateString}</span><span>{timeString}</span></div>
-    <div className={`border border-gray-200 p-2 rounded-lg ${mio ? ' rounded-br-none bg-theme-blue text-white' : 'rounded-bl-none'}`}>{mensaje.contenido}</div>
+  return <div className='flex flex-col'>
+    { !mio && <span className='ml-2 font-bold'>{`${mensaje.usuario.nombre} ${mensaje.usuario.apellido}`}</span>}
+    <div className={`flex ${mio ? 'flex-row self-end':'flex-row-reverse self-start'} gap-2 max-w-sm`}>
+      <div className='flex flex-col items-center text-[0.65rem] text-gray-400 justify-center'>
+        <span>{dateString}</span><span>{timeString}</span>
+      </div>
+      <div className={`border border-gray-200 p-2 rounded-lg ${mio ? ' rounded-br-none bg-theme-blue text-white' : 'rounded-bl-none'}`}>{mensaje.contenido}</div>
+    </div>
   </div>
 }
 
 const ChatsContainer = ({mensajes, usuarioActual}: IProperties) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if(containerRef.current){
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [mensajes]);
+  
   return (
-    <div className='flex-1 border-gray-300 border rounded-md w-full p-2 max-h-96 overflow-y-scroll'>
-      <div className='flex flex-col gap-2 max-h-full overflow-hidden'>
+    <div className='flex-1 border-gray-300 border rounded-md w-full p-2 max-h-96 overflow-y-scroll' ref={containerRef}>
+      <div className='flex flex-col gap-2 max-h-full'>
         {
-          mensajes.map(mensaje => <ChatItem key={mensaje.contenido} mio={usuarioActual === mensaje.usuario.email} mensaje = {mensaje}/>)
+          mensajes.map(mensaje => <ChatItem key={mensaje.id} mio={usuarioActual === mensaje.usuario.email} mensaje = {mensaje}/>)
         }
       </div>
     </div>
