396e747f52b2debf34472390c01a073d23bf33f0
Diego Quinteros <ql17001@ues.edu.sv>
2023-12-04 11:17:12 -0600

    ✨ Add mercure integration

--- app/chats/[id]/page.tsx
+++ app/chats/[id]/page.tsx
@@ -3,7 +3,8 @@ import ChatsContainer from '@/components/ChatsContainer'
 import { IFecha, IMensaje } from '@/types/global';
 import { retrieveAuthentication } from '@/utils/authentication';
 import customAxios from '@/utils/customAxios';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import {EventSourcePolyfill} from 'event-source-polyfill';
 
 interface IProperties {
   params: {
@@ -23,22 +24,58 @@ interface IChatResponse {
       id: number;
       fechaCreacion: IFecha;
       mensajes: IMensaje[]
+    },
+    mercure:{
+      'token': string,
+      'topic': string
     }
   }
 }
 
 const ChatPage = ({params: {id}}:IProperties) => {
   const [messages, setMessages] = useState<IMensaje[]>([]);
+  const [mercure, setMercure] = useState<IChatResponse['data']['mercure']>()
   const authentication = retrieveAuthentication();
+  const audioReference = useRef<HTMLAudioElement>(null)
+
+  useEffect(() => {
+    const url = new URL('https://localhost/.well-known/mercure');
+    url.searchParams.append('topic', mercure ? mercure.topic : '');
+
+    const es = new EventSourcePolyfill(url.toString(), {
+      headers: {
+          'Authorization': `Bearer ${mercure?.token}`,
+      }
+    });
+
+    es.onopen = event => {
+      console.log('Se conecto exitosamente.')
+    }
+
+    es.onmessage = (event) => {
+      const incomingMessage = JSON.parse(event.data) as IMensaje;
+      if(audioReference.current && incomingMessage.usuario.email !== authentication?.email){
+        audioReference.current.play();
+        console.log('Incoming message:', incomingMessage)
+        setMessages(previousMessages => [...previousMessages, incomingMessage])
+      }
+      
+    }
+  
+    return () => {
+      es.close()
+    }
+  }, [authentication?.email, mercure])
 
   useEffect(() => {
     const getChat = async () => {
       try {
         const response = await customAxios.get<IChatResponse>(`/chats/${id}`);
         const { data } = response;
-        const { data: { asesoria, chat: {mensajes} }, message } = data;
+        const { data: { asesoria, chat: {mensajes}, mercure }, message } = data;
 
         setMessages(mensajes)
+        setMercure(mercure);
 
       } catch (error) {
         
@@ -52,6 +89,7 @@ const ChatPage = ({params: {id}}:IProperties) => {
   return (
     <div className='flex flex-col flex-1 items-center w-full max-w-xl m-auto p-8 gap-2'>
       <h1>Usuario: Nombre asesoria</h1>
+      <audio ref={audioReference} src='/notification.wav' />
       <ChatsContainer mensajes={messages} usuarioActual={authentication ? authentication.email : ''}/>
       <div className='w-full flex flex-row gap-4'>
         <input className='flex-1' type="text" name="" id="" />
